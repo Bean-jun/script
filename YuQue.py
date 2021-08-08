cafc39d6290c617344aa33d43ffbadef76bb3e1a
Bean-jun <1342104001@qq.com>
2021-08-08 14:05:00 +0800

    去除_request冗余

--- YuQue.py
+++ YuQue.py
@@ -22,19 +22,12 @@ class YuQueConnect:
     def _request(self, method, path, **kwargs):
         try:
             self.headers['X-Auth-Token'] = self.token
-            if method == "post":
-                response = requests.post(self.BASE_URL + path, data=json.dumps(kwargs), headers=self.headers)
-            elif method == 'put':
-                response = requests.put(self.BASE_URL + path, data=json.dumps(kwargs), headers=self.headers)
+
+            if isinstance(method, str):
+                response = requests.request(method.upper(), self.BASE_URL + path, data=json.dumps(kwargs), headers=self.headers)
             else:
-                req = """requests.{method}("{url}", data={data}, headers={headers})""".format(method=method,
-                                                                                              url=str(
-                                                                                                  self.BASE_URL + path
-                                                                                              ),
-                                                                                              # data=kwargs,
-                                                                                              data=json.dumps(kwargs),
-                                                                                              headers=self.headers)
-                response = eval(req)
+                raise Exception("error")
+
         except Exception as e:
             raise e
 
